e03fc2ae683e90511d72fa5a74289cd665f48ad8
Hanatan <hanatan079@gmail.com>
2022-12-13 16:01:46 +0900

    位置を指定

--- GetBallGame/Assets/Scripts/BallScript.cs
+++ GetBallGame/Assets/Scripts/BallScript.cs
@@ -10,13 +10,14 @@ public class BallScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //ランダムに取得
+        //5~15の間でランダムなスピード
         speed = Random.Range(5f,15f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //位置を指定(-1→手前に転がす為)
+        transform.position += new Vector3(0f, 0f, -1 * speed * Time.deltaTime);
     }
 }
