2fe609d0ca44094d54fdb5942a3645412c450471
Hanatan <hanatan079@gmail.com>
2022-12-13 15:49:12 +0900

    ランダムに取得

--- GetBallGame/Assets/Scripts/BallScript.cs
+++ GetBallGame/Assets/Scripts/BallScript.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
+
+    private float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //ランダムに取得
+        speed = Random.Range(5f,15f);
     }
 
     // Update is called once per frame
