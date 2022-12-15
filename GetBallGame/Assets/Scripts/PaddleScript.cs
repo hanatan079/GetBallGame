9562db81be8b0fc9474785e8cc67a685da39b5d9
Hanatan <hanatan079@gmail.com>
2022-12-15 12:54:06 +0900

    パドルの移動範囲設定

--- GetBallGame/Assets/Scripts/PaddleScript.cs
+++ GetBallGame/Assets/Scripts/PaddleScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PaddleScript : MonoBehaviour
 {
     public float speed;
+
+    //パドルの移動範囲
     public float xRange =5;
 
 
@@ -17,6 +19,16 @@ public class PaddleScript : MonoBehaviour
     {
         //矢印キーの押下でパドルを左右に動かせるようにする (deltatime前回のフレームからの経過時間)
         transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f, 0f);
+
+        //パドルの移動範囲設定
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange,  transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
 
     }
 }
