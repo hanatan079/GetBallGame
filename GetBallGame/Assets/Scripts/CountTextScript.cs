1ab0bba1911a260f3f64ed1168d534c0bdd11f9f
Hanatan <hanatan079@gmail.com>
2022-12-17 10:54:04 +0900

    ゲームエンジン変更

--- GetBallGame/Assets/Scripts/CountTextScript.cs
+++ GetBallGame/Assets/Scripts/CountTextScript.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+//Text Mesh Proのエンジン
+using TMPro;
+
 
 public class CountTextScript : MonoBehaviour
 {
     int score = 0;
-    public Text scoreText;
+    public TextMeshProUGUI scoreText;
 
     public void ADDScore()
     {
         score++;
+    }
+
+    void update()
+    {
+        scoreText.text = "SCORE:" + score.ToString();
     }
 }
