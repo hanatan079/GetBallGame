2f83b6351050c8da3e91b2fb29c4eb8cf3a0cb60
Hanatan <hanatan079@gmail.com>
2022-12-20 08:58:30 +0900

    経過時間初期化

--- GetBallGame/Assets/Scripts/AutomaticMovement.cs
+++ GetBallGame/Assets/Scripts/AutomaticMovement.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AutomaticMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //経過時間初期化
+        step_time = 0.0f;
     }
 
     // Update is called once per frame
