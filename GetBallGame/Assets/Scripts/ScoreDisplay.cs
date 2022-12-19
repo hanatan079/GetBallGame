0242f25d7067f7748b162745bc8f795a128ef6cd
Hanatan <hanatan079@gmail.com>
2022-12-19 11:53:34 +0900

    getScore関数呼び出し

--- GetBallGame/Assets/Scripts/ScoreDisplay.cs
+++ GetBallGame/Assets/Scripts/ScoreDisplay.cs
@@ -14,7 +14,7 @@ public class ScoreDisplay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+         score = ScoreScript.getScore();
     }
 
     // Update is called once per frame
